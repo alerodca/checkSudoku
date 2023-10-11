ae255d35c4aac218648691a6af2aea0a36246073
Alejandro Rodríguez Cañete <ale.rodca04@gmail.com>
2023-10-11 23:05:08 +0200

    here detects errors horizontally

--- Sudoku.playground/Contents.swift
+++ Sudoku.playground/Contents.swift
@@ -8,3 +8,11 @@ var arrayBidimensional: [[Int]] = [
     [5,1,2,3,4]
 ]
 
+for insideArray in arrayBidimensional {
+    let countedSet = NSCountedSet(array: insideArray)
+    for value in countedSet {
+        if (countedSet.count(for: value) != 1) {
+            print("El valor \(value) se encuentra repetido")
+        }
+    }
+}
