b53b05438bd63ec5870d25b76b566ed77b91de24
Alejandro Rodríguez Cañete <ale.rodca04@gmail.com>
2023-10-12 17:08:47 +0200

    checks vertically and horizontally, and is embedded in a function

--- Sudoku.playground/Contents.swift
+++ Sudoku.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// Making a sudoku that will check
 var arrayBidimensional: [[Int]] = [
     [1,2,3,4,5],
     [2,3,4,5,1],
@@ -8,11 +9,47 @@ var arrayBidimensional: [[Int]] = [
     [5,1,2,3,4]
 ]
 
-for insideArray in arrayBidimensional {
-    let countedSet = NSCountedSet(array: insideArray)
-    for value in countedSet {
-        if (countedSet.count(for: value) != 1) {
-            print("El valor \(value) se encuentra repetido")
+checkSudoku(arrayBidimensional: arrayBidimensional)
+
+func checkSudoku(arrayBidimensional: [[Int]]) {
+    var isCorrect: Bool = true
+    var numbersSetHorizontaly: NSCountedSet
+    var numbersSetVerticaly: NSCountedSet
+    var finalBidimensionalArray = arrayBidimensional.count-1
+    var verticalLine: [Int] = []
+
+    for insideArray in arrayBidimensional {
+        numbersSetHorizontaly = NSCountedSet(array: insideArray)
+        for value in numbersSetHorizontaly {
+            if (value as! Int == 0) {
+                print("ERROR. El valor mínimo debe ser 1")
+            } else {
+                if (numbersSetHorizontaly.count(for: value) != 1) {
+                    isCorrect = false
+                    print("El valor \(value) se encuentra repetido horizontalmente")
+                }
+            }
         }
+    }
+    for i in 0...finalBidimensionalArray {
+        verticalLine = []
+        for j in 0...arrayBidimensional[i].count-1 {
+            verticalLine.append(arrayBidimensional[j][i])
+        }
+        numbersSetVerticaly = NSCountedSet(array: verticalLine)
+        for value in numbersSetVerticaly {
+            if (value as! Int == 0) {
+                print("ERROR. El valor mínimo debe ser 1")
+            } else {
+                if (numbersSetVerticaly.count(for: value) != 1) {
+                    isCorrect = false
+                    print("El valor \(value) se encuentra repetido verticalmente")
+                }
+            }
+        }
+    }
+    
+    if isCorrect {
+        print("¡FELICIDADES, el sudoku no tiene fallos!")
     }
 }
